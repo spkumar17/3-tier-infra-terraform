0a56c5b7d7d0579073d140c940ef7872940d17f4
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-06-28 11:45:58 +0530

    MYSQL ENDPOINT

--- Module/ASG/userdata.sh
+++ Module/ASG/userdata.sh
@@ -22,6 +22,8 @@ sleep 5
 sudo docker pull prasannakumarsinganamalla431/petclinic:23
 sleep 5
 
+export MYSQL_URL="${var.db_instance_endpoint}"
+
 # Run an Ubuntu container with MySQL environment variables
 sudo docker run -d --name petclinic -e MYSQL_URL=jdbc:mysql://${MYSQL_URL}/petclinic -e MYSQL_USER=petclinic -e MYSQL_PASSWORD=petclinic -e MYSQL_ROOT_PASSWORD=petclinic -p 8888:8888 prasannakumarsinganamalla431/petclinic:23
 
