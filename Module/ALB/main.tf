ca8baf118a0bf91dc59c1df588d72d2bb8d431bb
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-06-29 23:51:38 +0530

    userdata script updated

--- Module/ALB/main.tf
+++ Module/ALB/main.tf
@@ -1,3 +1,26 @@
+## create target group
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "${var.project_name}-tg1"
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = 200
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 # alb resource 
 
 resource "aws_lb" "alb" {
@@ -30,26 +53,3 @@ resource "aws_lb_listener" "alb_http_listener" {
   
 }
 
-## create target group
-
-resource "aws_lb_target_group" "alb_target_group" {
-  name        = "${var.project_name}-tg1"
-  target_type = "instance"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-
-  health_check {
-    enabled             = true
-    interval            = 300
-    path                = "/"
-    timeout             = 60
-    matcher             = 200
-    healthy_threshold   = 2
-    unhealthy_threshold = 5
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
